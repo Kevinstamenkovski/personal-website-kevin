0b4015459508a9d185d0c8e0907a8c85f94fa3d1
kevin stamenkovski <kevin.stamenkovski@gmail.com>
2022-07-12 22:35:39 +0200

    added skill Section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,21 @@ import {HomeComponent} from "./components/home/home.component";
 import {ContactComponent} from "./components/contact/contact.component";
 import {EducationComponent} from "./components/education/education.component";
 import {ProfileComponent} from "./components/profile/profile.component";
+import {SkillsComponent} from "./components/skills/skills.component";
+
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'contact', component: ContactComponent},
   { path: 'education', component: EducationComponent},
   { path: 'profile', component: ProfileComponent},
+  { path: 'skills', component: SkillsComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
-export const comps = [ HomeComponent, ContactComponent, EducationComponent, ProfileComponent ]
+export const comps = [ HomeComponent, ContactComponent, EducationComponent, ProfileComponent, SkillsComponent ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { MatDividerModule } from "@angular/material/divider";
-import { comps } from "./app-routing.module";
+import {AppRoutingModule, comps} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SidenavComponent} from './sidenav/sidenav.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatDividerModule} from "@angular/material/divider";
 
 
 @NgModule({
