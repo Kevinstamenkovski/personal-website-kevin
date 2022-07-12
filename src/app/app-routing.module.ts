8cc7355f7633f7a71f68a671793435f315cdefa5
kevin stamenkovski <kevin.stamenkovski@gmail.com>
2022-07-12 18:59:30 +0200

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {ContactComponent} from "./components/contact/contact.component";
+import {EducationComponent} from "./components/education/education.component";
+import {ProfileComponent} from "./components/profile/profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'education', component: EducationComponent},
+  { path: 'profile', component: ProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const comps = [ HomeComponent, ContactComponent, EducationComponent, ProfileComponent ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
-import {MatDividerModule} from "@angular/material/divider";
-
-
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatDividerModule } from "@angular/material/divider";
+import { comps } from "./app-routing.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidenavComponent
+    SidenavComponent,
+    comps
   ],
   imports: [
     BrowserModule,
